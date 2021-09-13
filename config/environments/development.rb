f8a5bed02536faf76cf4a1dc12cd20e578fc285e
AndreyChim <chimitcyrenov@mail.ru>
2021-09-13 21:27:24 +0800

    modify development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+    # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
@@ -74,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
