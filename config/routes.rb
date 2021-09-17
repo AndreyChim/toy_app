ae0fced05bcf041f9454dcb9f9899265348d6b04
AndreyChim <chimitcyrenov@mail.ru>
2021-09-17 21:51:10 +0800

    modified appl.html.erb & routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
+  root 'users#index'
+
+  get 'home/index'
   #devise_for :users
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'users#index'
 end
